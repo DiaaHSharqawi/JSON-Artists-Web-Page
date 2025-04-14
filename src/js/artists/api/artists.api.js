2ca72bee912c07ba1e7a4274c74fb9aed18b0b95
DiaaHSharqawi <diaasharqawi00@gmail.com>
2025-04-14 14:51:27 +0300

    feat: add loading spinner to artist section while data is being fetched

--- src/js/artists/api/artists.api.js
+++ src/js/artists/api/artists.api.js
@@ -2,7 +2,18 @@ const API_URL =
   "https://gist.githubusercontent.com/planetoftheweb/98f35786733c8cccf81e/raw/f3dad774ed1fe20b36011b1261bb392ee759b867/data.json";
 
 export default async function getArtists() {
-  const response = await fetch(API_URL);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      console.error(response);
+      throw new Error(`Faild to fetch!`);
+    }
+    const data = await response.json();
+    console.info(data);
+    return data;
+  } catch (error) {
+    console.error(`An error happend!`);
+    console.error(error);
+    return null;
+  }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -5,21 +5,39 @@ import {
 } from "./artists/helpers/artists.helpers.js";
 
 let artistsSection = document.getElementById("artists-section");
+const loader = document.getElementById("loader");
+
+function triggerLoader(isLoading) {
+  artistsSection.style.display = isLoading ? "none" : "flex";
+  loader.style.display = isLoading ? "flex" : "none";
+}
 
 document.addEventListener("DOMContentLoaded", async () => {
   const artistsElement = document.createElement("div");
   artistsElement.setAttribute("class", "artists");
   artistsSection.appendChild(artistsElement);
 
-  const artists = await getArtists();
+  triggerLoader(true);
 
-  artists.forEach((artist) => {
-    const artistCardElement = createArtistElement();
-    artistsElement.appendChild(artistCardElement);
+  try {
+    const artists = await getArtists();
+    const artistsHasContent = artists.length > 0;
+    if (artistsHasContent) {
+      setTimeout(() => {
+        triggerLoader(false);
+      }, 500);
+      artists.forEach((artist) => {
+        const artistCardElement = createArtistElement();
+        artistsElement.appendChild(artistCardElement);
 
-    const atistCardComponents = getArtistCardComponents(artist);
-    atistCardComponents.forEach((component) => {
-      artistCardElement.appendChild(component);
-    });
-  });
+        const atistCardComponents = getArtistCardComponents(artist);
+        atistCardComponents.forEach((component) => {
+          artistCardElement.appendChild(component);
+        });
+      });
+    }
+  } catch (error) {
+    console.error(`An error happen!`);
+    return;
+  }
 });
